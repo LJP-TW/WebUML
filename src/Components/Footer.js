f154afc0fb4cfe612cc9ce717122b14950cd6afe
Shung <jordan880805@gmail.com>
2020-01-06 04:42:27 +0800

    Mac dock like menu

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,24 +1,27 @@
 import React, { useState, useEffect } from "react";
-import setUMLObjs from "./setUMLObjs"
-
+import setUMLObjs from "./setUMLObjs";
+import "../Css/dock.css"
 export default function Footer(props) {
-    const objLists = "objectLists";
+	const objLists = "objectLists";
 
-    // Handle parent call
-    useEffect(() => {
-        if (props.parentCall !== null) {
-            if (props.parentCall.toLowerCase() === 'setumlobjs') {
-                setUMLObjs(props.graph, objLists);
-            }
-        }
-    }, [props.parentCall]);
+	// Handle parent call
+	useEffect(() => {
+		if (props.parentCall !== null) {
+			if (props.parentCall.toLowerCase() === "setumlobjs") {
+				setUMLObjs(props.graph, objLists);
+			}
+		}
+	}, [props.parentCall]);
 
-    return (
-        <div id={props.id}>
-            <div class="float_center">
-                <ul id={objLists}>
-                </ul>
-            </div>
-        </div>
-    );
+	return (
+		<div id={props.id}>
+			<div id="dock-container">
+				<div id="dock">
+					<div class="float_center">
+						<ul id={objLists}></ul>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 }
